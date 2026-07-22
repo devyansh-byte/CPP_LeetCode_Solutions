26784a6f469a82796ce60612a6640ff9ff1a0a0f
devyansh-byte <guptadevu8009145077@gmail.com>
2026-07-23 00:32:29 +0530

    Time: 8 ms (46.69%), Space: 128.4 MB (54.42%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& arr) {
-        //brute force solution.
         int n=arr.size();
-        vector<int> pos;
-        vector<int> neg;
-        for(int i=0;i<n;i++){
-            if(arr[i]>0) pos.push_back(arr[i]);
-            else neg.push_back(arr[i]);
-        }
-        // one containing positive ele and other negative.
-        vector<int> ans;
-        for(int i=0;i<n/2;i++){
-           ans.push_back(pos[i]);
-            ans.push_back(neg[i]);
+        vector<int> ans(n);
+        int i=0;
+        int j=1;
+        int x=0;
+        while(x<n){
+           if(arr[x]>0){
+            ans[i]=arr[x];
+            i+=2;
+           }
+           else{
+            ans[j]=arr[x];
+            j+=2;
+           }
+           x++;
         }
         return ans;
+    
     }
 };
