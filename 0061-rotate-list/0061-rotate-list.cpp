6badae06f58fe1f559e8af2fc78dde4482113bbc
devyansh-byte <guptadevu8009145077@gmail.com>
2026-08-01 15:04:36 +0530

    Time: 0 ms (100%), Space: 16.4 MB (64.85%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,41 +1,25 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        ListNode* temp=head;
         if(head==NULL || head->next==NULL) return head;
-        if(head->next->next==NULL){
-            if(k%2!=0){
-                head->next->next=head;
-              head=head->next;
-              temp->next=NULL;
-              return head;
-            }
-            else return head;
-        }
-        int n=1;
-        while(temp!=NULL && temp->next!=NULL){
+            ListNode* temp=head;
+            ListNode* tail=head;
+        int n=0;
+        while(temp!=NULL){
+            if(temp->next==NULL) tail=temp;
             temp=temp->next;
             n++;
         }
-         if(k%n==0) return head;
-      temp->next=head;
-      //now we create a cycle.
-      if(k%n==0) return head;
-      int m=k%n;
-      int idx=n-m+1;
-       temp=head;
-      int cnt=1;
-      while(cnt<idx){
-        head=head->next;
-        if(cnt<idx-1) temp=temp->next;
-        cnt++;
-      }
-         ListNode* temp2=head;
-      while(temp2!=temp){
-        temp2=temp2->next;
-      }
-      temp2->next=NULL;
-            return head;
+     k=k%n;
+     temp=head;
+     if(k==0) return head;
+     for(int i=1;i<n-k;i++){
+        temp=temp->next;
+     }
+     tail->next=head;
+     head=temp->next;
+     temp->next=NULL;
+     return head;
     }
    
 };
