dc9cf2b1022b280fb4a74f8dfcb383fce0b63fd3
devyansh-byte <guptadevu8009145077@gmail.com>
2026-08-13 22:38:35 +0530

    Time: 0 ms (100%), Space: 8.3 MB (74.61%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,15 +2,29 @@ class Solution {
 public:
     int minimumPushes(string arr) {
         int n=arr.size();
-        int count=0;
-        int counter=0;
-        for(int i=0;i<n;i++){
-            if(counter<8) count++;
-            else if(counter<16) count+=2;
-            else if(counter<24) count+=3;
-            else count+=4;
-            counter++;
-        }
-    return count;
+          int count=8;
+        if(n<9) return n;
+        else{
+            n=n-8;
+            if(n<9){
+                count=count+(n*2);
+                return count;
+            }
+            else{
+                 count=count+(8*2);
+                 n=n-8;
+                 if(n<9){
+                    count=count+(n*3);
+                 } 
+                 else{
+                     count=count+(8*3);
+                     n=n-8;
+                     count=count+(n*4);
+                 }
+            }
+
+        } 
+        return count;
+        
     }
 };
