71971fa9e4314cbbf2fe277b76a9951314882f94
devyansh-byte <guptadevu8009145077@gmail.com>
2026-09-09 02:34:50 +0530

    Time: 14 ms (99.39%), Space: 82.8 MB (89.15%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        for(int i=1;i<n;i++){
+        if(arr[i]-arr[i-1]==0) return true;
+        }
+        return false;
+    }
+};
